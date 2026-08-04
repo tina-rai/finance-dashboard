6ecded4b01e383bbdbc89553d557eefd8c869ab1
Tina <contactina101@gmail.com>
2026-08-04 21:24:47 +0545

    Initialize Personal Finance Dashboard project

---
+++ js/dom.js
@@ -0,0 +1,23 @@
+const form =
+document.getElementById("transaction-form");
+
+const descriptionInput =
+document.getElementById("description");
+
+const amountInput =
+document.getElementById("amount");
+
+const typeSelect =
+document.getElementById("type");
+
+const balance =
+document.getElementById("balance");
+
+const income =
+document.getElementById("income");
+
+const expense =
+document.getElementById("expense");
+
+const transactionList =
+document.getElementById("transaction-list");
